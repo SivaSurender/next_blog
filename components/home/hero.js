4e1b5a04c9ed990ff0b745cc5d35a366e470c0b9
SivaSurender <e.surender14@gmail.com>
2025-01-19 18:36:01 +0530

    hero

--- components/home/hero.js
+++ components/home/hero.js
@@ -1,3 +1,21 @@
+import Image from "next/image";
+import classes from "./hero.module.css";
 export default function Hero() {
-  return <div>Hero</div>;
+  return (
+    <section className={classes.hero}>
+      <div className={classes.image}>
+        <Image
+          src="/site/image/pic.jpg"
+          alt="profile image"
+          width={300}
+          height={300}
+        />{" "}
+      </div>
+      <h1>Hi, I'm Bala</h1>
+      <p>
+        I try to blog about web development especially js frameworks like React
+        or Next
+      </p>
+    </section>
+  );
 }
