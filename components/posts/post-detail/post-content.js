5f462e2651464d46b652fd48121a6d42300a7bf2
bala_e <e.surender14@gmail.com>
2025-01-21 12:37:58 +0530

    syntax-highlighter and styles for viewing individual blog posts

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -2,22 +2,62 @@ import React from "react";
 import PostHeader from "./post-header";
 import classes from "./post-content.module.css";
 import ReactMarkdown from "react-markdown";
+import Image from "next/image";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
+// const DUMMY_POST = {
+//   slug: "getting-started-nextjs",
+//   title: "Getting started with next",
+//   content: "# This is a sample post",
+//   image: "getting-started-nextjs.png",
 
-const DUMMY_POST = {
-  slug: "getting-started-nextjs",
-  title: "Getting started with next",
-  content: "# This is a sample post",
-  image: "getting-started-nextjs.png",
+//   date: "2025-04-20",
+// };
 
-  date: "2025-04-20",
-};
+export default function PostContent({ post }) {
+  const customRenderers = {
+    // img(image) {
+    //   console.log({ s: `${image.src}` }, "fdgdgf");
+    //   return (
+    //     <Image src={`${image.src}`} alt={image.alt} width={600} height={300} />
+    //   );
+    // },
+    p(paragraph) {
+      const { node } = paragraph;
 
-export default function PostContent() {
-  const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
+      if (node.children[0].tagName === "img") {
+        const image = node.children[0];
+
+        return (
+          <div className={classes.image}>
+            {" "}
+            <Image
+              src={`${image.properties.src}`}
+              alt={image.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+
+      return <p>{paragraph.children}</p>;
+    },
+    code({ className, children }) {
+      return (
+        <SyntaxHighlighter
+          language={className.split("-")[1]}
+          children={children}
+          style={atomDark}
+        />
+      );
+    },
+  };
+  const imagePath = `/images/posts/${post.slug}/${post.image}`;
   return (
     <article className={classes.content}>
-      <PostHeader title={DUMMY_POST.title} image={imagePath} />
-      <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
+      <PostHeader title={post.title} image={imagePath} />
+      <ReactMarkdown components={customRenderers}>{post.content}</ReactMarkdown>
     </article>
   );
 }
--- pages/posts/[postSlug].js
+++
@@ -1,7 +0,0 @@
-import PostContent from "../../components/posts/post-detail/post-content";
-
-function PostDetial() {
-  return <PostContent />;
-}
-
-export default PostDetial;
