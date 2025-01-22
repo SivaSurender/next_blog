9a2995e4f93df9683c608f3b14b7f232df3e47aa
SivaSurender <e.surender14@gmail.com>
2025-01-22 19:02:28 +0530

    meta and document

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import FeaturedPosts from "../components/home/featured-posts";
 import Hero from "../components/home/Hero";
 import { getFeaturedPosts } from "../lib/posts-util";
@@ -5,6 +6,13 @@ import { getFeaturedPosts } from "../lib/posts-util";
 export default function HomePage(props) {
   return (
     <>
+      <Head>
+        <title>Bala's Blog</title>
+        <meta
+          name="description"
+          content="Programming and web development posts."
+        />
+      </Head>
       <Hero />
       <FeaturedPosts posts={props.posts} />
     </>
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import AllPosts from "../../components/posts/all-posts";
 import { getAllPosts } from "../../lib/posts-util";
 // const DUMMY_POSTS = [
@@ -39,7 +40,18 @@ import { getAllPosts } from "../../lib/posts-util";
 //   },
 // ];
 export default function AllPostsPage(props) {
-  return <AllPosts posts={props.posts} />;
+  return (
+    <>
+      <Head>
+        <title>All Posts</title>
+        <meta
+          name="description"
+          content="A list of all programming tutorials and posts"
+        />
+      </Head>
+      <AllPosts posts={props.posts} />
+    </>
+  );
 }
 export function getStaticProps() {
   const allPosts = getAllPosts();
